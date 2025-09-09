4195d6e7a3a3189de887a45cf31c4b2f3c35b8ed
aziraliu <31254684+aziraliu@users.noreply.github.com>
2025-09-09 12:16:55 +0200

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from the North Macedonia!";
 console.log(hello());
 exports.hello = hello;
